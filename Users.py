aa754c470e2a54b13bf03aece57d76d7fe236a76
Jacob-MH <149729134+Jacob-MH@users.noreply.github.com>
2023-11-25 12:46:21 -0600

    Update Users.py
    Signed-off-by: Jacob-MH <149729134+Jacob-MH@users.noreply.github.com>

--- Users.py
+++ Users.py
@@ -1,6 +1,13 @@
 class Users:
     def __init__(self):
         self.userID = None
+        self.password = None
+        self.firstName = None
+        self.lastName = None
+        self.address = None
+        self.state = None
+        self.zip = None
+        self.payment = None
         self.loggedIn = False
         self.tableName = None
         self.databaseName = None
@@ -20,8 +27,15 @@ class Users:
         if self.loggedIn == True:
             print("You are already logged in, please logout to create an account.")
         else:
-            self.userID = input("Enter your FirstName-LastName")
-            print("Your UserID is {self.userID}.")
+            self.email = input("Enter your email: ")
+            self.password = input("Create a new password: ")
+            self.firstName = input("Enter your First Name: ")
+            self.lastName = input("Enter your Last name: ")
+            self.address = input("Enter your address: ")
+            self.state = input("Enter your state: ")
+            self.zip = input("Enter your zip: ")
+            self.payment = input("Enter your payment type: ")
+            self.AccountInformation = {UserID, email, password, firstName, lastName, address, city, state, zip, payment}
             self.loggedIn = True
             print("Account creation successful.")
     
@@ -30,4 +44,3 @@ class Users:
 
     def getUserID(self):
         return self.UserID
-
