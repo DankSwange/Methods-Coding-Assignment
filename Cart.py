8139a422901f8610f2119a05f7b747660619f653
Luke Stafford <149811055+staffordl@users.noreply.github.com>
2023-11-25 22:45:06 -0600

    Update Cart.py
    Signed-off-by: Luke Stafford <149811055+staffordl@users.noreply.github.com>

--- Cart.py
+++ Cart.py
@@ -1,3 +1,18 @@
+import sqlite3
+import sys
+
+try:
+    connection = sqlite3.connect("Inventory.db")
+
+except:
+    print("Failed connection.")
+
+    sys.exit()
+
+cursor = connection.cursor()
+
+
+
 class Cart:
     databaseName = ''
     tableName = ''
@@ -10,10 +25,23 @@ class Cart:
         self.databaseName = databaseName
         self.tableName = tableName
 
-    def viewCart(userID, inventoryDatabase)
+    def viewCart(self, userID, inventoryDatabase):
+        query = "SELECT * FROM ? WHERE ISBN IN (SELECT ISBN FROM Cart WHERE userID=?)"
+        data = (inventoryDatabase, userID)  # this could cause an error
+        cursor.execute(query, data)
+
+        result = cursor.fetchall()
+
+        print("Your cart contents: \n")
+
+        for x in result:
+            Print() #FINISH
+
+    def addToCart(self, userID, ISBN):
+        return
+
+    def removeFromCart(self, userID, ISBN):
+        return
     
-    def addToCart(userID, ISBN)
-    
-    def removeFromCart(userID, ISBN)
-        
-    def checkOut(userID)
+    def checkOut(self, userID):
+        return
