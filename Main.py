12a5b3fd4302c24b9b1640feea40ec7d31de23df
DankSwange <149728891+DankSwange@users.noreply.github.com>
2023-11-27 17:39:11 -0600

    Update Main.py
    Signed-off-by: DankSwange <149728891+DankSwange@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
+inventoryOption = 0
+while (inventoryOption != 1):
+  print("1. Go back\n")
+  print("2. View Inventory\n")
+  print("3. Search Inventory\n")
+  inventoryOption = input("Please select a valid option from the menu above 1-3: ")
+  while (inventoryOption < 1 and inventoryOption > 3):
+    inventoryOption = input("Please select a valid option from the menu above 1-3: ")
 
+  
